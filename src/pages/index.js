443e6fae828082152c939a427c2cb56bfc516462
KeithC113 <campbell_ka@yahoo.co.uk>
2020-11-24 16:15:02 +0000

    about page rendering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
-}
+  return (
+  
+  <div style={{ color: `purple` }}>
+    
+    <h1>Hello Keith.. </h1>
+    
+      <p> and what a world it is!! </p>
+      
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    
+    </div>
+  )}
